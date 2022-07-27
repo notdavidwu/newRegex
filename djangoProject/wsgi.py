0328d4df766644e1950ae38ce6326026f4c07ac0
DaYi-TW <66512110+DaYi-TW@users.noreply.github.com>
2022-07-27 10:27:29 +0800

    add finctions in eventDefinitions page

--- djangoProject/wsgi.py
+++ djangoProject/wsgi.py
@@ -6,11 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject.settings')
+if os.environ.get('DEV') is True:
+   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+else:
+   os.environ.setdefault("DJANGO_SETTINGS_MODULE", 
+   "config.settings.production")
 
 application = get_wsgi_application()
